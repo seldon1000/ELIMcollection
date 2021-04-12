18ef4a6af878c634a30caae76b66b4c05cdff385
Nicolas Mariniello <seldon1000@tutanota.com>
2021-04-12 11:22:43 +0200

    Update houghLines.cpp
    Optimized lines generation

--- houghLines.cpp
+++ houghLines.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 using namespace cv;
 
-vector<Vec2d> houghLines(Mat src, int T)
+Mat srcColor;
+
+void houghLines(Mat src, int T)
 {
 	double R = src.rows > src.cols ? src.rows : src.cols;
 	int Rho = R * sqrt(2.0), Theta = 180;
@@ -26,45 +28,38 @@ vector<Vec2d> houghLines(Mat src, int T)
 		}
 	}
 
-	vector<Vec2d> lines;
-
 	for (int i = 0; i < Rho; i++)
 	{
 		for (int j = 0; j < Theta; j++)
 		{
 			if (acc.at<uchar>(i, j) > T)
-				lines.push_back(Vec2d(i, j * M_PI / 180));
+			{
+				double A = cos(j * M_PI / 180);
+				double B = sin(j * M_PI / 180);
+				double x0 = A * i;
+				double y0 = B * i;
+
+				Point p1, p2;
+				p1.x = x0 + 1000 * -B;
+				p1.y = y0 + 1000 * A;
+				p2.x = x0 - 1000 * -B;
+				p2.y = y0 - 1000 * A;
+
+				line(srcColor, p1, p2, Scalar(0, 0, 255), 1, 8, 0);
+			}
 		}
 	}
-
-	return lines;
 }
 
 int main()
 {
 	Mat src = imread("./Images/road.jpg", 0);
-	Mat srcColor = imread("./Images/road.jpg");
+	srcColor = imread("./Images/road.jpg");
 
 	blur(src, src, Size(7, 7));
 	Canny(src, src, 50, 150, 3);
 
-	vector<Vec2d> lines = houghLines(src, 80);
-
-	for (int i = 0; i < lines.size(); i++)
-	{
-		double A = cos(lines[i][1]);
-		double B = sin(lines[i][1]);
-		double x0 = A * lines[i][0];
-		double y0 = B * lines[i][0];
-
-		Point p1, p2;
-		p1.x = x0 + 1000 * -B;
-		p1.y = y0 + 1000 * A;
-		p2.x = x0 - 1000 * -B;
-		p2.y = y0 - 1000 * A;
-
-		line(srcColor, p1, p2, Scalar(0, 0, 255), 1, 8, 0);
-	}
+	houghLines(src, 80);
 
 	imshow("Hough Lines", srcColor);
 	waitKey(0);
